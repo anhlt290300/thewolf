44433e9ccf9e05195c9a87ea3ecab20d01cae4ad
anhlt290300 <70874794+anhlt290300@users.noreply.github.com>
2023-04-19 16:15:18 +0700

    19/4

--- client/src/assets/fakeData/sliderData.js
+++ client/src/assets/fakeData/sliderData.js
@@ -8,3 +8,14 @@ export const sliderData = [
         href:'/collections/giay-nam-the-wolf'
     },
 ]
+
+export const sliderPhoneData = [
+    {
+        src: 'https://file.hstatic.net/200000033444/file/banner_phone_1_263713ac588c4414bb3c71ae421d94b3.jpg',
+        href:'/collections/giay-nam-the-wolf'
+    },
+    {
+        src: 'https://file.hstatic.net/200000033444/file/banner_phone_2_63812278f1ac49f59cb867029c8b15fb.jpg',
+        href:'/collections/giay-nam-the-wolf'
+    },
+]
